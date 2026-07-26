93d4adc43396329c4dba6871e8568b93f69209f1
chohanraza81-rgb <chohanraza81@gmail.com>
2026-07-26 23:56:48 +0500

    Create errorHandler.js

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,6 @@
+const logger = require('../config/logger');
+function errorHandler(err, req, res, next) {
+  logger.error(err.stack);
+  res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
+}
+module.exports = errorHandler;
