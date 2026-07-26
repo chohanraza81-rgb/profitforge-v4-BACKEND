356e041830839d0d93126c661cb1c4fc27215d8f
chohanraza81-rgb <chohanraza81@gmail.com>
2026-07-26 23:47:10 +0500

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: String,
+  role: { type: String, default: 'user' },
+  preferences: Object,
+}, { timestamps: true });
+module.exports = mongoose.model('User', UserSchema);
