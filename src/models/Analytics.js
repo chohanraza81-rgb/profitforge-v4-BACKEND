cf6e0ea37e7679d4f969ceb41e40e15500309673
chohanraza81-rgb <chohanraza81@gmail.com>
2026-07-26 23:51:05 +0500

    Create Analytics.js

---
+++ src/models/Analytics.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const AnalyticsSchema = new mongoose.Schema({
+  userId: String,
+  action: String,
+  metadata: Object,
+  timestamp: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model('Analytics', AnalyticsSchema);
