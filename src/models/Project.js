308dead4ac6dc99232b29712d6d35e23b0d3da26
chohanraza81-rgb <chohanraza81@gmail.com>
2026-07-27 07:16:45 +0500

    Update Project.js

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+
 const ProjectSchema = new mongoose.Schema({
   userId: { type: String, required: true },
   keyword: { type: String, required: true },
   data: { type: Object, required: true },
 }, { timestamps: true });
+
 module.exports = mongoose.model('Project', ProjectSchema);
