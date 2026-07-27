c327659aaf9a546cfbe9cb2dbd4aacc3c61c81b0
chohanraza81-rgb <chohanraza81@gmail.com>
2026-07-27 20:45:46 +0500

    Update index.js

--- src/services/dataProviders/index.js
+++ src/services/dataProviders/index.js
@@ -1,19 +1,10 @@
 const serpApi = require('./serpApi');
-const apify = require('./apify');
 const scrapingBee = require('./scrapingBee');
-const amazonScraper = require('./amazonScraper');   // ✅ new
-const shopify = require('./shopify');
-const googleShopping = require('./googleShopping');
-const ebay = require('./ebay');
-const etsy = require('./etsy');
+const amazonScraper = require('./amazonScraper');
+// Groq is not a data provider; it's used in scoringEngine
 
 module.exports = {
   serpApi,
-  apify,
   scrapingBee,
   amazonScraper,
-  shopify,
-  googleShopping,
-  ebay,
-  etsy
 };
