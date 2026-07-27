1e4f289b231689bb778cd206177442ea6ecc8ab3
chohanraza81-rgb <chohanraza81@gmail.com>
2026-07-27 07:18:25 +0500

    Update Analytics.js

--- src/models/Analytics.js
+++ src/models/Analytics.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+
 const AnalyticsSchema = new mongoose.Schema({
   userId: String,
   action: String,
   metadata: Object,
   timestamp: { type: Date, default: Date.now },
 });
+
 module.exports = mongoose.model('Analytics', AnalyticsSchema);
